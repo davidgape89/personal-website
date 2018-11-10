58d9ec7e9bda54cd9dfcc04134a39f62ba6ed67a
davidgape89 <davidgape89@gmail.com>
2018-11-10 12:10:57 +0100

    feat(app): Set up app module and component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', loadChildren: './home/home-page.module#HomePageModule'},
+  {path: 'resume', loadChildren: './resume/resume-page.module#ResumePageModule'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
